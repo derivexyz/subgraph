f2d9ed3390f80dd887a3cc2b8e99391b10315a46
paulvaden <pbdonut@gmail.com>
2022-05-23 10:38:59 -0400

    external swap fee updates

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,10 @@ graphNetwork=$network
 
 if [ $network = 'kovan-ovm' ]; then
   graphNetwork='optimism-kovan'
+  subgraphName=$KOVAN_SUBGRAPH_NAME
 elif [ $network = 'mainnet-ovm' ]; then
   graphNetwork='optimism'
+  subgraphName=$SUBGRAPH_NAME
 fi
 
-NETWORK=$graphNetwork $GRAPH deploy $SUBGRAPH_NAME --product hosted-service --access-token $ACCESS_TOKEN --node https://api.thegraph.com/deploy/ --ipfs https://api.thegraph.com/ipfs/ --network $network --type $type subgraph.js
+NETWORK=$graphNetwork $GRAPH deploy $subgraphName --product hosted-service --access-token $ACCESS_TOKEN --node https://api.thegraph.com/deploy/ --ipfs https://api.thegraph.com/ipfs/ --network $network --type $type subgraph.js
--- src/mappings/optionMarketWrapper.ts
+++ src/mappings/optionMarketWrapper.ts
@@ -1,4 +1,4 @@
-import { Trade } from '../../generated/schema'
+import { CollateralUpdate, Trade } from '../../generated/schema'
 import { PositionTraded } from '../../generated/templates/OptionMarketWrapper/OptionMarketWrapper'
 import { Entity, ZERO } from '../lib'
 
@@ -7,11 +7,24 @@ export function handlePositionTraded(event: PositionTraded): void {
   if (event.params.swapFee != ZERO) {
     let marketId = Entity.getIDFromAddress(event.params.market)
     let positionId = Entity.getPositionID(marketId, event.params.positionId.toI32())
+    if (event.params.isLong) {
+      let tradeid = Entity.getTradeIDFromPositionID(positionId, event.transaction.hash)
+      let trade = Trade.load(tradeid) as Trade
 
-    let tradeid = Entity.getTradeIDFromPositionID(positionId, event.transaction.hash)
-    let trade = Trade.load(tradeid) as Trade
+      trade.externalSwapFees = event.params.swapFee
+      trade.externalSwapAddress = event.params.token
+      trade.save()
+    } else {
+      let collateralUpdateID = Entity.getCollateralUpdateID(
+        marketId,
+        event.params.positionId.toI32(),
+        event.transaction.hash,
+      )
+      let collateralUpdate = CollateralUpdate.load(collateralUpdateID) as CollateralUpdate
 
-    trade.externalSwapFees = event.params.swapFee
-    trade.save()
+      collateralUpdate.externalSwapFees = event.params.swapFee
+      collateralUpdate.externalSwapAddress = event.params.token
+      collateralUpdate.save()
+    }
   }
 }
